0376d45162c98318f3e0318eebd36afb7b20d0ac
Koen Sparreboom <koensparreboom@gmail.com>
2018-01-02 22:21:53 +0100

    Added a titlebar

--- js/script.js
+++ js/script.js
@@ -70,3 +70,18 @@ holder.ondrop = (e) => {
 
     return false;
 };
+
+var autoHideTitlebarInterval;
+
+$('*').mousemove(function(e) {
+    clearInterval(autoHideTitlebarInterval);
+
+    if (e.clientY <= $('#titlebar')[0].getBoundingClientRect().bottom) {
+        $('#titlebar').css('opacity', '1');
+        autoHideTitlebarInterval = setInterval(function () {
+            $('#titlebar').css('opacity', '0');
+        }, 10000);
+    } else {
+        $('#titlebar').css('opacity', '0');
+    }
+  })
