cbb2c12048f53d8201248f99cbe1e0c0e0afa6a6
Kaitlin <katiesparreboom@gmail.com>
2024-05-10 21:16:27 +0200

    Moved code to script.js & fixed a 404

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const remote = require('electron').remote;
+const ipc = require('electron').ipcRenderer;
+
+const electronScreen = require('electron').screen;
+const screenSize = electronScreen.getPrimaryDisplay().size;
+
+var picture = document.getElementById('picture');
+
+remote.getCurrentWindow().setSize(picture.width, picture.height);
+remote.getCurrentWindow().setPosition(Math.round(screenSize.width / 2 - picture.width / 2), Math.round(screenSize.height / 2 - picture.height / 2));
+
+$('#minimizeButton').click(function () {
+    remote.getCurrentWindow().minimize();
+});
+
+$('#closeButton').click(function () {
+    remote.getCurrentWindow().close();
+});
+
+$('#select-directory').click(function (event) {
+    ipc.send('open-file-dialog');
+});
+
+ipc.on('selected-directory', function (event, path) {
+    picture.src = `${path}`;
+});
+
+$('#picture').on('load', function () {
+    remote.getCurrentWindow().setSize(picture.width, picture.height);
+    remote.getCurrentWindow().setPosition(Math.round(screenSize.width / 2 - picture.width / 2), Math.round(screenSize.height / 2 - picture.height / 2));
+});
